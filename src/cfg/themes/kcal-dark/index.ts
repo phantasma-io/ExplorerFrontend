9f2e1ffed7f1a1fcff3e6e580b91ae361838a7a7
l1b3r_- <liber@ricardojrm.com>
2022-01-18 12:06:28 +0000

    feat: kcal dark mode config

---
+++ src/cfg/themes/kcal-dark/index.ts
@@ -0,0 +1,19 @@
+import { ThemeOptions } from '@mui/material';
+import { typography } from '../typography';
+import { overrides } from '../overrides';
+
+export const kcalDark: ThemeOptions = {
+  typography,
+  palette: {
+    mode: 'dark',
+    primary: {
+      light: '#d02525',
+      main: '#911818',
+      dark: '#661111',
+    },
+    secondary: {
+      main: '#d02525',
+    },
+  },
+  components: overrides,
+};
