90133f29751c9789c03fd7133bcce852d47ff14c
l1b3r_- <liber@ricardojrm.com>
2022-01-18 12:07:32 +0000

    perf: export theme configs

---
+++ src/cfg/index.ts
@@ -0,0 +1,7 @@
+export { FOOTER_HEIGHT, FOOTER_OFFSET, HEADER_HEIGHT } from './layout';
+
+export { locales } from './locales';
+
+export { metas } from './metas';
+
+export { themes } from './themes';
