41c6da65ab8f83e9a39f6730e4a6bab576f6748f
l1b3r_- <liber@ricardojrm.com>
2022-01-18 12:06:10 +0000

    feat: kcal theme config

---
+++ src/cfg/themes/kcal/index.ts
@@ -0,0 +1,18 @@
+import { ThemeOptions } from '@mui/material';
+import { typography } from '../typography';
+import { overrides } from '../overrides';
+
+export const kcal: ThemeOptions = {
+  typography,
+  palette: {
+    primary: {
+      light: '#d85050',
+      main: '#d02525',
+      dark: '#911818',
+    },
+    secondary: {
+      main: '#d02525',
+    },
+  },
+  components: overrides,
+};
