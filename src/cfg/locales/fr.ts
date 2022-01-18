a67c40874493ef6b2af021c83dbbc20604d73891
l1b3r_- <liber@ricardojrm.com>
2022-01-18 12:02:32 +0000

    feat: init french locale

---
+++ src/cfg/locales/fr.ts
@@ -0,0 +1,31 @@
+import { EchoMsgs } from '@ricardo-jrm/echo';
+
+export const fr: EchoMsgs = {
+  // sys
+  example: 'exemple fr',
+  wip: '🚧 Travaux en cours 🚧',
+  'copy-to-clipboard': 'Copier dans le Presse-papiers',
+  'copied-to-clipboard': 'Copié dans le Presse-papiers',
+  'not-found': 'Introuvable',
+  close: 'Fermer',
+  apply: 'Appliquer',
+  search: 'Rechercher',
+  // app
+  account: 'Compte',
+  address: 'Adresse',
+  hash: 'Hash',
+  block: 'Bloquer',
+  chain: 'Chaîne',
+  contract: 'Contrat',
+  dao: 'DAO',
+  token: 'Jeton',
+  transaction: 'Transaction',
+  // meta
+  'meta-description': 'Meta description', // TODO
+  // tooltips
+  'tooltip-locale': 'Changer de langue',
+  'tooltip-darkmode': 'Basculer en mode sombre',
+  'tooltip-search': 'Ouvrir la recherche',
+  'tooltip-brand': 'Changer de marque',
+  'tooltip-nav-homepage': "Aller à l'accueil",
+};
