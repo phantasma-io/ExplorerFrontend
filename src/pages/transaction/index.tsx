f52477cca10b8f69a64329b96035c188d60e3e8d
l1b3r_- <liber@ricardojrm.com>
2022-01-19 18:44:21 +0000

    feat(pages): transaction

---
+++ src/pages/transaction/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { ViewPage } from '../../containers/ViewPage';
+
+export const TransactionPage: NextPage = () => (
+  <ViewPage title="title-transaction" />
+);
+
+export default TransactionPage;
