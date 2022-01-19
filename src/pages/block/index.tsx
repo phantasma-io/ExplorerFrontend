1aefe592f7dc19a4bd04d8f77287146057a950ba
l1b3r_- <liber@ricardojrm.com>
2022-01-19 18:42:53 +0000

    feat(pages): block

---
+++ src/pages/block/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { ViewPage } from '../../containers/ViewPage';
+
+export const BlockPage: NextPage = () => <ViewPage title="title-block" />;
+
+export default BlockPage;
