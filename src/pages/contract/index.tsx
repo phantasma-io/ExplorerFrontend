006e43a1133f8e924c5242bda4d409f2669a7c8c
l1b3r_- <liber@ricardojrm.com>
2022-01-19 18:43:23 +0000

    feat(pages): contract

---
+++ src/pages/contract/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { ViewPage } from '../../containers/ViewPage';
+
+export const ContractPage: NextPage = () => <ViewPage title="title-contract" />;
+
+export default ContractPage;
