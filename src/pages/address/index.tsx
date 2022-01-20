2ce0bdaa54675a33201f8c8ccc720a035478bfe9
l1b3r_- <liber@ricardojrm.com>
2022-01-20 16:32:51 +0000

    feat(views): address tabs

--- src/pages/address/index.tsx
+++ src/pages/address/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { ViewPage } from '../../containers/ViewPage';
+import { ViewAddress } from '../../containers/ViewAddress';
 
-export const AddressPage: NextPage = () => <ViewPage title="title-address" />;
+export const AddressPage: NextPage = () => (
+  <ViewPage title="title-address">
+    <ViewAddress />
+  </ViewPage>
+);
 
 export default AddressPage;
