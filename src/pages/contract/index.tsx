c4202c67ef2455b9a00a8320f787e69ac0442ca9
l1b3r_- <liber@ricardojrm.com>
2022-01-20 16:33:56 +0000

    feat(views): contract tabs

--- src/pages/contract/index.tsx
+++ src/pages/contract/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { ViewPage } from '../../containers/ViewPage';
+import { ViewContract } from '../../containers/ViewContract';
 
-export const ContractPage: NextPage = () => <ViewPage title="title-contract" />;
+export const ContractPage: NextPage = () => (
+  <ViewPage title="title-contract">
+    <ViewContract />
+  </ViewPage>
+);
 
 export default ContractPage;
