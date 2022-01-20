a3d7fae95b7cc988b8e6f7843a3e3169ca7217b6
l1b3r_- <liber@ricardojrm.com>
2022-01-20 07:19:19 +0000

    feat(components): 404

--- src/containers/ViewPage/index.tsx
+++ src/containers/ViewPage/index.tsx
@@ -12,7 +12,7 @@ export const ViewPage = ({ children, title }: ViewPageProps) => {
   const { echo } = useEcho();
   return (
     <Box>
-      <Box pt={{ xs: 3, lg: 9 }}>
+      <Box pt={{ xs: 3, lg: 6 }}>
         <Text variant="h2" sx={{ color: '#fff' }}>
           {echo(title)}
         </Text>
