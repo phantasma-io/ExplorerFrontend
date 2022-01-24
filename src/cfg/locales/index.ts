ef8e1097efd560ed4cc5aa69eb0fede71dfc9358
l1b3r_- <liber@ricardojrm.com>
2022-01-24 19:36:10 +0000

    feat(locales): improved config

--- src/cfg/locales/index.ts
+++ src/cfg/locales/index.ts
@@ -1,12 +1,22 @@
-import { EchoRecord } from '@ricardo-jrm/echo';
+import { EchoMsgs } from '@ricardo-jrm/echo';
 import { en } from './en';
 import { pt } from './pt';
 import { de } from './de';
 import { fr } from './fr';
 
-export const locales: EchoRecord = {
+export type Locales = 'en' | 'pt' | 'de' | 'fr';
+
+type LocalesRecord = {
+  [key in Locales]: EchoMsgs;
+};
+
+export const locales: LocalesRecord = {
   en,
   pt,
   de,
   fr,
 };
+
+export const localesKeys = Object.keys(locales);
+
+export const localesDefault: Locales = localesKeys[0] as Locales;
