b3a80999dc3a3626b91428aaca69b46a5d8dd7bf
l1b3r_- <liber@ricardojrm.com>
2022-01-24 19:36:47 +0000

    feat(routes): localized routing

--- src/cfg/routes/index.ts
+++ src/cfg/routes/index.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { objToQuery } from '../../scripts/objQuery';
+import { localesKeys, Locales, localesDefault } from '../locales';
 
-type ExplorerRoutes =
+export type ExplorerRoutes =
   | '/'
   | '/nexus'
   | '/chain'
@@ -16,17 +18,32 @@ type RouterParams = {
 };
 
 type ExplorerRouter = {
-  [key in ExplorerRoutes]: (params?: RouterParams) => string;
+  [key in ExplorerRoutes]: (params?: RouterParams, locale?: Locales) => string;
 };
 
 export const routes: ExplorerRouter = {
-  '/': () => '/',
-  '/nexus': () => '/nexus',
-  '/chain': (params) => `/chain${params ? objToQuery(params) : ''}`,
-  '/address': (params) => `/address${params ? objToQuery(params) : ''}`,
-  '/block': (params) => `/block${params ? objToQuery(params) : ''}`,
-  '/contract': (params) => `/contract${params ? objToQuery(params) : ''}`,
-  '/dao': (params) => `/dao${params ? objToQuery(params) : ''}`,
-  '/token': (params) => `/token${params ? objToQuery(params) : ''}`,
-  '/transaction': (params) => `/transaction${params ? objToQuery(params) : ''}`,
+  '/': (params, locale = localesDefault) => `/${locale}/`,
+  '/nexus': (params, locale = localesDefault) => `/${locale}/nexus`,
+  '/chain': (params, locale = localesDefault) =>
+    `/${locale}/chain${params ? objToQuery(params) : ''}`,
+  '/address': (params, locale = localesDefault) =>
+    `/${locale}/address${params ? objToQuery(params) : ''}`,
+  '/block': (params, locale = localesDefault) =>
+    `/${locale}/block${params ? objToQuery(params) : ''}`,
+  '/contract': (params, locale = localesDefault) =>
+    `/${locale}/contract${params ? objToQuery(params) : ''}`,
+  '/dao': (params, locale = localesDefault) =>
+    `/${locale}/dao${params ? objToQuery(params) : ''}`,
+  '/token': (params, locale = localesDefault) =>
+    `/${locale}/token${params ? objToQuery(params) : ''}`,
+  '/transaction': (params, locale = localesDefault) =>
+    `/${locale}/transaction${params ? objToQuery(params) : ''}`,
 };
+
+export const routesKeys = Object.keys(routes);
+
+export const routesViews = routesKeys.map((key) =>
+  key.substring(1, key.length),
+);
+
+export const routesHome = (() => localesKeys.map((key) => `/${key}`))();
