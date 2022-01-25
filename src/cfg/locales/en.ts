6a68f280125a241e1bc2c3e1ff7208004e3e42a7
l1b3r_- <liber@ricardojrm.com>
2022-01-25 16:16:10 +0000

    feat(locales): english labels

--- src/cfg/locales/en.ts
+++ src/cfg/locales/en.ts
@@ -40,25 +40,25 @@ export const en: EchoMsgs = {
   // btns
   'btn-enterNexus': 'Enter the Nexus',
   // page titles
-  'title-nexus': 'Nexus', // TODO
-  'title-chain': 'chain', // TODO
-  'title-address': 'address', // TODO
-  'title-block': 'block', // TODO
-  'title-transaction': 'transaction', // TODO
-  'title-contract': 'contract', // TODO
-  'title-token': 'token', // TODO
-  'title-dao': 'dao', // TODO
+  'title-nexus': 'Welcome to the Nexus',
+  'title-chain': 'Explore chain',
+  'title-address': 'Explore address',
+  'title-block': 'Explore block',
+  'title-transaction': 'Explore transaction',
+  'title-contract': 'Explore contract',
+  'title-token': 'Explore token',
+  'title-dao': 'Explore DAO',
   // tabs
-  'tab-overview': 'Overview', // TODO
-  'tab-chains': 'Chains', // TODO
-  'tab-tokens': 'Tokens', // TODO
-  'tab-daos': 'Daos', // TODO
-  'tab-blocks': 'Blocks', // TODO
-  'tab-contracts': 'Contracts', // TODO
-  'tab-balances': 'Balances', // TODO
-  'tab-transactions': 'Transactions', // TODO
-  'tab-events': 'Events', // TODO
-  'tab-oracles': 'Oracles', // TODO
-  'tab-script': 'Script', // TODO
-  'tab-members': 'Members', // TODO
+  'tab-overview': 'Overview',
+  'tab-chains': 'Chains',
+  'tab-tokens': 'Tokens',
+  'tab-daos': 'DAOs',
+  'tab-blocks': 'Blocks',
+  'tab-contracts': 'Contracts',
+  'tab-balances': 'Balances',
+  'tab-transactions': 'Transactions',
+  'tab-events': 'Events',
+  'tab-oracles': 'Oracles',
+  'tab-script': 'Script',
+  'tab-members': 'Members',
 };
