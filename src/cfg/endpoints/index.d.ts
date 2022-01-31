e80f608c3542ec9bb1c3d7da6cb1399fb10bc1f9
l1b3r_- <liber@ricardojrm.com>
2022-01-31 21:20:18 +0000

    feat(endpoints): types and definitions

---
+++ src/cfg/endpoints/index.d.ts
@@ -0,0 +1,22 @@
+type ExplorerEndpoints =
+  | '/addresses'
+  | '/chains'
+  | '/contracts'
+  | '/instructions'
+  | '/eventkinds'
+  | '/events'
+  | '/nfts'
+  | '/series'
+  | '/tokens'
+  | '/transactions';
+
+interface ListParams {
+  order_by?: string;
+  order_direction?: 'asc' | 'desc';
+  offset?: number;
+  limit?: number;
+  with_total?: number;
+}
+interface ListResults {
+  total_results?: number;
+}
---
+++ src/cfg/endpoints/index.ts
@@ -0,0 +1,3 @@
+import { objToQuery } from '../../scripts/objQuery';
+
+const url = 'http://88.99.141.166:8000/api/v1';
