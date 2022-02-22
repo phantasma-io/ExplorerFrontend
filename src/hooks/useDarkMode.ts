526aa464efb5762c48f73986be365f78d830ee2e
l1b3r_- <liber@ricardojrm.com>
2022-02-22 07:55:26 +0000

    feat(hooks): use dark mode

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,18 @@
+import { useMemo, useCallback } from 'react';
+import { useFury } from '@ricardo-jrm/fury';
+
+export const useDarkMode = () => {
+  const { furyActiveId, furySetById } = useFury();
+
+  const isDark = useMemo(() => furyActiveId.includes('-dark'), [furyActiveId]);
+
+  const toggleDarkMode = useCallback(() => {
+    if (isDark) {
+      furySetById(furyActiveId.split('-')[0]);
+    } else {
+      furySetById(`${furyActiveId}-dark`);
+    }
+  }, [isDark, furyActiveId, furySetById]);
+
+  return { isDark, toggleDarkMode };
+};
