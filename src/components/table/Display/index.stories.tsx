f48cc2543494986504984edb90c9f8dd47cf5129
l1b3r_- <liber@ricardojrm.com>
2022-02-22 07:55:52 +0000

    feat(table): details dialog

--- src/components/table/Display/index.stories.tsx
+++ src/components/table/Display/index.stories.tsx
@@ -20,24 +20,45 @@ export default {
 
 const cols: TableDisplayCol[] = [
   {
+    id: 'Col1',
     label: 'Col1',
     size: 4,
     cell: 'text',
   },
   {
+    id: 'Col2',
     label: 'Col2',
     size: 4,
     cell: 'number',
   },
   {
+    id: 'Col3',
     label: 'Col3',
     size: 4,
     cell: 'text',
   },
 ];
 
-const rows: TableDisplayRow[] = [['Cell1', 1337, 'Cell3']];
+const rows: TableDisplayRow[] = [
+  ['Cell1', 1337, 'Cell3'],
+  ['Cell1', undefined, 'Cell3'],
+  ['Cell1', 1337, 'Cell3'],
+  ['Cell1', 1337, 'Cell3'],
+  ['Cell1', 1337, 'Cell3'],
+  ['Cell1', 1337, 'Cell3'],
+];
 
 export const Default: Story<TableDisplayProps> = () => {
   return <TableDisplay rows={rows} cols={cols} total={1000} />;
+};
+
+export const WithDetails: Story<TableDisplayProps> = () => {
+  return (
+    <TableDisplay
+      rows={rows}
+      cols={cols}
+      total={1000}
+      withDetails={{ title: 'Details', action: 'close' }}
+    />
+  );
 };
--- src/components/table/Display/index.tsx
+++ src/components/table/Display/index.tsx
@@ -3,13 +3,19 @@ import { Box } from '@mui/material';
 import { TableController } from '../Controller';
 import { TableDisplayDesktop } from './desktop';
 import { TableDisplayMobile } from './mobile';
-import { TableDisplayData } from './types';
+import { TableDisplayData, DetailSchema } from './types';
 
 export interface TableDisplayProps extends TableDisplayData {
   total: number;
+  withDetails?: DetailSchema;
 }
 
-export const TableDisplay = ({ rows, cols, total }: TableDisplayProps) => {
+export const TableDisplay = ({
+  rows,
+  cols,
+  total,
+  withDetails,
+}: TableDisplayProps) => {
   return (
     <Box p={1}>
       <Box mb={1}>
@@ -17,7 +23,11 @@ export const TableDisplay = ({ rows, cols, total }: TableDisplayProps) => {
       </Box>
       {/* desktop */}
       <Box mb={1} display={{ xs: 'none', md: 'block' }}>
-        <TableDisplayDesktop rows={rows} cols={cols} />
+        <TableDisplayDesktop
+          rows={rows}
+          cols={cols}
+          withDetails={withDetails}
+        />
       </Box>
       {/* mobile */}
       <Box mb={1} display={{ xs: 'block', md: 'none' }}>
