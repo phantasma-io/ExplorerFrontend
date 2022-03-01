d78555d58e2a2245e5d91da4080e9d27d77451a1
l1b3r_- <liber@ricardojrm.com>
2022-03-01 09:01:49 +0000

    feat(list): daos

---
+++ src/components/list/Daos/index.tsx
@@ -0,0 +1,63 @@
+import React, { useMemo } from 'react';
+import { useEcho } from '@ricardo-jrm/echo';
+import { useEmpathy } from '@ricardo-jrm/empathy';
+import { Box } from '@mui/material';
+import { endpoints, TABLE_FILTERS } from 'cfg';
+import { useTable } from 'hooks';
+import { TableDisplayCol, TableDisplayRow } from 'types/table';
+import { DaoResults } from 'types/api';
+import { Table } from 'components/table';
+
+export const DaosList = () => {
+  const { echo } = useEcho();
+
+  const tableProps = useTable();
+  const { limit, order_by, order_direction, offset, with_total } = tableProps;
+
+  const { data } = useEmpathy<DaoResults>(
+    endpoints['/organizations']({
+      offset,
+      limit,
+      order_by,
+      order_direction,
+      with_total,
+    }),
+  );
+
+  const cols = useMemo<TableDisplayCol[]>(
+    () => [
+      {
+        id: 'name',
+        label: echo('name'),
+        cell: 'text',
+        size: 11,
+        showDesktop: true,
+      },
+    ],
+    [echo],
+  );
+
+  const rows = useMemo<TableDisplayRow[]>(() => {
+    if (data) {
+      return data?.organizations?.map((item) => [
+        item?.name,
+      ]) as TableDisplayRow[];
+    }
+
+    return [];
+  }, [data]);
+
+  return (
+    <Box>
+      <Table
+        tableId="PhantasmaExplorer-Daos"
+        raw={data?.organizations || []}
+        cols={cols}
+        rows={rows}
+        total={data?.total_results || 0}
+        {...tableProps}
+        filters={TABLE_FILTERS}
+      />
+    </Box>
+  );
+};
