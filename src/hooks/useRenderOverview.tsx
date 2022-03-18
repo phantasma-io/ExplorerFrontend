877ed808669e4da60e6bc4bd1c0b29edcee9d2a6
l1b3r_- <liber@ricardojrm.com>
2022-03-18 10:44:08 +0000

    feat(hooks): render overview

---
+++ src/hooks/useRenderOverview.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback } from 'react';
+import { nanoid } from 'nanoid';
+import { Box, Grid } from '@mui/material';
+import { useRenderDetails } from 'hooks/useRenderDetails';
+import { TableDisplayCol, TableDisplayRow } from 'types/table';
+
+export const useRenderOverview = () => {
+  const renderDetails = useRenderDetails();
+
+  const renderOverview = useCallback(
+    (cols: TableDisplayCol[], rows: TableDisplayRow[]) => (
+      <Box>
+        <Grid spacing={1} container>
+          {cols.map((col, i) =>
+            rows[0][i] ? (
+              <Grid
+                container
+                spacing={1}
+                key={nanoid()}
+                item
+                xs={12}
+                alignItems="center"
+              >
+                <Grid item container>
+                  {renderDetails(
+                    col.type,
+                    rows[0][i],
+                    col.label,
+                    col.linkOptions,
+                  )}
+                </Grid>
+              </Grid>
+            ) : null,
+          )}
+        </Grid>
+      </Box>
+    ),
+    [renderDetails],
+  );
+
+  return renderOverview;
+};
