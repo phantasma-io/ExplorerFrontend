d97fb638b862dfe2340c26bbdb337c979834a2f5
l1b3r_- <liber@ricardojrm.com>
2022-05-04 08:56:44 +0100

    feat: improve nft details

--- src/hooks/api/nfts.ts
+++ src/hooks/api/nfts.ts
@@ -24,7 +24,7 @@ export const useNftData = (data?: NftResults, loading?: boolean) => {
         type: 'thumbnail',
         size: 2,
       },
-      // metadata
+      // data
       {
         id: 'name',
         label: echo('name'),
@@ -45,11 +45,16 @@ export const useNftData = (data?: NftResults, loading?: boolean) => {
         size: 2,
       },
       {
-        id: 'contract',
-        label: echo('contract'),
-        type: 'text',
-        size: 2,
-        showDesktop: true,
+        id: 'current_supply',
+        label: echo('current_supply'),
+        type: 'number',
+        size: 3,
+      },
+      {
+        id: 'max_supply',
+        label: echo('max_supply'),
+        type: 'number',
+        size: 3,
       },
       {
         id: 'mint_number',
@@ -66,24 +71,30 @@ export const useNftData = (data?: NftResults, loading?: boolean) => {
         showDesktop: true,
       },
       {
-        id: 'rom',
-        label: echo('rom'),
-        type: 'script',
+        id: 'contract',
+        label: echo('contract'),
+        type: 'text',
         size: 2,
+        showDesktop: true,
       },
-      {
-        id: 'ram',
-        label: echo('ram'),
-        type: 'script',
-        size: 2,
-      },
-      // root
       {
         id: 'symbol',
         label: echo('symbol'),
         type: 'text',
         size: 3,
       },
+      // {
+      //   id: 'rom',
+      //   label: echo('rom'),
+      //   type: 'script',
+      //   size: 2,
+      // },
+      // {
+      //   id: 'ram',
+      //   label: echo('ram'),
+      //   type: 'script',
+      //   size: 2,
+      // },
       {
         id: 'creator_onchain_name',
         label: echo('creator_onchain_name'),
@@ -101,54 +112,41 @@ export const useNftData = (data?: NftResults, loading?: boolean) => {
           title: echo('explore-address'),
         },
       },
-      {
-        id: 'chain',
-        label: echo('chain'),
-        type: 'text',
-        size: 2,
-      },
-      // series
-      {
-        id: 'series_name',
-        label: echo('series_name'),
-        type: 'text',
-        size: 3,
-      },
-      {
-        id: 'series_desc',
-        label: echo('series_desc'),
-        type: 'text',
-        size: 5,
-      },
-      {
-        id: 'series_creator',
-        label: echo('series_creator'),
-        type: 'monospace',
-        size: 1,
-        linkOptions: {
-          route: '/address',
-          key: 'address',
-          title: echo('explore-address'),
-        },
-      },
-      {
-        id: 'current_supply',
-        label: echo('current_supply'),
-        type: 'number',
-        size: 3,
-      },
-      {
-        id: 'max_supply',
-        label: echo('max_supply'),
-        type: 'number',
-        size: 3,
-      },
-      {
-        id: 'image',
-        label: echo('image'),
-        type: 'monospace',
-        size: 5,
-      },
+      // {
+      //   id: 'chain',
+      //   label: echo('chain'),
+      //   type: 'text',
+      //   size: 2,
+      // },
+      // {
+      //   id: 'series_name',
+      //   label: echo('series_name'),
+      //   type: 'text',
+      //   size: 3,
+      // },
+      // {
+      //   id: 'series_desc',
+      //   label: echo('series_desc'),
+      //   type: 'text',
+      //   size: 5,
+      // },
+      // {
+      //   id: 'series_creator',
+      //   label: echo('series_creator'),
+      //   type: 'monospace',
+      //   size: 1,
+      //   linkOptions: {
+      //     route: '/address',
+      //     key: 'address',
+      //     title: echo('explore-address'),
+      //   },
+      // },
+      // {
+      //   id: 'image',
+      //   label: echo('image'),
+      //   type: 'monospace',
+      //   size: 5,
+      // },
       {
         id: 'mode_name',
         label: echo('mode_name'),
@@ -209,30 +207,29 @@ export const useNftData = (data?: NftResults, loading?: boolean) => {
   const rows = useMemo<TableDisplayRow[]>(() => {
     if (data) {
       return data?.nfts?.map((item) => [
+        // thumb
         item?.nft_metadata?.image,
-        // metadata
+        // data
         item?.nft_metadata?.name,
         item?.nft_metadata?.description,
         item?.nft_metadata?.image,
-        item?.contract?.name,
+        item?.series?.current_supply,
+        item?.series?.max_supply,
         item?.nft_metadata?.mint_number,
         item?.nft_metadata?.mint_date
           ? unixmsToDate(item?.nft_metadata?.mint_date)
           : undefined,
-        item?.nft_metadata?.rom,
-        item?.nft_metadata?.ram,
-        // root
+        item?.contract?.name,
         item?.symbol,
+        // item?.nft_metadata?.rom,
+        // item?.nft_metadata?.ram,
         item?.creator_onchain_name,
         item?.creator_address,
-        item?.chain,
-        // series
-        item?.series?.name,
-        item?.series?.description,
-        item?.series?.creator,
-        item?.series?.current_supply,
-        item?.series?.max_supply,
-        item?.series?.image,
+        // item?.chain,
+        // item?.series?.name,
+        // item?.series?.description,
+        // item?.series?.creator,
+        // item?.series?.image,
         item?.series?.mode_name,
         item?.series?.type,
         item?.series?.royalties,
