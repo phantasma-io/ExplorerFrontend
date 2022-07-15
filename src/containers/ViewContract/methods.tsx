5a0c56d83b6da683b77a04bedbbec8311bbf272a
ricardojrmcom <liber@ricardojrm.com>
2022-07-15 15:13:53 +0100

    feat(contract): methods tab

---
+++ src/containers/ViewContract/methods.tsx
@@ -0,0 +1,42 @@
+import React, { useMemo } from 'react';
+import { Box } from '@mui/material';
+import { ContractResults } from 'types/api';
+import { Loading, Error, Empty } from 'components/layout';
+import { DetailsScript } from 'components/details';
+
+export interface ContractMethodsProps {
+  data?: ContractResults;
+  loading?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error?: any;
+}
+
+export const ContractMethods = ({
+  data,
+  loading,
+  error,
+}: ContractMethodsProps) => {
+  const content = useMemo(() => {
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error || data?.error) {
+      return <Error />;
+    }
+
+    if (data?.contracts?.length === 0 && !loading) {
+      return <Empty />;
+    }
+
+    return (
+      <Box>
+        {data?.contracts && data?.contracts[0]?.methods && (
+          <DetailsScript value={JSON.stringify(data?.contracts[0]?.methods)} />
+        )}
+      </Box>
+    );
+  }, [loading, error, data]);
+
+  return <Box p={1}>{content}</Box>;
+};
