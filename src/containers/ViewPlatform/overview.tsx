23829f01be1ab2a37fde783f7ae4258e5f01d894
ricardojrmcom <liber@ricardojrm.com>
2022-07-15 15:18:58 +0100

    feat(platform): overview tab

---
+++ src/containers/ViewPlatform/overview.tsx
@@ -0,0 +1,49 @@
+import React, { useMemo } from 'react';
+import { nanoid } from 'nanoid';
+import { Box } from '@mui/material';
+import { useRenderOverview } from 'hooks/useRenderOverview';
+import { usePlatformData } from 'hooks/api';
+import { PlatformResults } from 'types/api';
+import { Loading, Error, Empty, Overview } from 'components/layout';
+
+export interface PlatformOverviewProps {
+  data?: PlatformResults;
+  loading?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error?: any;
+}
+
+export const PlatformOverview = ({
+  data,
+  loading,
+  error,
+}: PlatformOverviewProps) => {
+  const renderOverview = useRenderOverview();
+
+  const { cols, rows, raw } = usePlatformData(data);
+
+  const content = useMemo(() => {
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error || data?.error) {
+      return <Error />;
+    }
+
+    if (rows.length === 0 && !loading) {
+      return <Empty />;
+    }
+
+    return (
+      <Overview
+        csvFilename={`PhantasmaExplorer-Platform-${nanoid()}.csv`}
+        raw={raw[0]}
+      >
+        <Box>{data && renderOverview(cols, rows)}</Box>
+      </Overview>
+    );
+  }, [loading, error, rows, data, renderOverview, cols, raw]);
+
+  return <Box p={1}>{content}</Box>;
+};
