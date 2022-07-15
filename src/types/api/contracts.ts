53e0a5c12312a6ead2e25abfa73127e833d42ac7
ricardojrmcom <liber@ricardojrm.com>
2022-07-15 15:23:30 +0100

    feat(types): update contracts

--- src/types/api/contracts.ts
+++ src/types/api/contracts.ts
@@ -1,14 +1,26 @@
-import { ListParams, ListResults } from './list';
+import { ListParams, ListResults, WithOption } from './list';
+import { Address } from './addresses';
+import { Token } from './tokens';
 
 export interface Contract {
   name?: string;
   hash?: string;
   symbol?: string;
+  address?: Address;
+  script_raw?: string;
+  token?: Token;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  methods?: any;
 }
 
 export interface ContractParams
   extends ListParams,
-    Pick<Contract, 'symbol' | 'hash'> {}
+    Pick<Contract, 'symbol' | 'hash'> {
+  with_methods?: WithOption;
+  with_script?: WithOption;
+  with_token?: WithOption;
+  with_creation_event?: WithOption;
+}
 
 export interface ContractResults extends ListResults {
   contracts?: Contract[];
