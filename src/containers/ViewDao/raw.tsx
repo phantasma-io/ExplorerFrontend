dd1457c4d9b6314841ba4e61a3f96d7662af08f7
ricardojrmcom <liber@ricardojrm.com>
2022-07-15 15:17:17 +0100

    feat(dao): raw tab

---
+++ src/containers/ViewDao/raw.tsx
@@ -0,0 +1,32 @@
+import React, { useMemo } from 'react';
+import { Box } from '@mui/material';
+import { DaoResults } from 'types/api';
+import { Loading, Error, Empty } from 'components/layout';
+import { DetailsScript } from 'components/details';
+
+export interface DaoRawProps {
+  data?: DaoResults;
+  loading?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error?: any;
+}
+
+export const DaoRaw = ({ data, loading, error }: DaoRawProps) => {
+  const content = useMemo(() => {
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error || data?.error) {
+      return <Error />;
+    }
+
+    if (data?.organizations?.length === 0 && !loading) {
+      return <Empty />;
+    }
+
+    return <Box>{data && <DetailsScript value={JSON.stringify(data)} />}</Box>;
+  }, [loading, error, data]);
+
+  return <Box p={1}>{content}</Box>;
+};
