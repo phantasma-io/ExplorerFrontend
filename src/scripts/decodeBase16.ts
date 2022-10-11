15f5f6dd78291b623352e429b65fb9f1d1161a2c
ricardojrmcom <liber@ricardojrm.com>
2022-10-11 16:11:01 +0100

    feat: decode base 16 script

---
+++ src/scripts/decodeBase16.ts
@@ -0,0 +1,7 @@
+export const decodeBase16 = (hex: string) => {
+  let str = '';
+  for (let i = 0; i < hex.length; i += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return str;
+};
