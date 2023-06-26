36be89a51eb2c7859c04c9a3969c03fc45cf16c6
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-06-26 07:45:01 +0100

    Update index.ts

--- src/cfg/endpoints/index.ts
+++ src/cfg/endpoints/index.ts
@@ -7,7 +7,7 @@ const parseParams = (params: ApiParams | undefined) =>
 const testnet = process.env.NEXT_PUBLIC_TESTNET === 'testnet';
 
 export const url = `https://${testnet ? 'testnet' : 'ngexplorer'}.phantasma.io${
-  testnet ? '' : ''
+  testnet ? '8001' : ''
 }/api/v1`;
 
 export const endpoints: ExplorerApi = {
