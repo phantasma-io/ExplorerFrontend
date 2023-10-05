49de67e4d91877559122c1caef6e9acb6dbb977e
alexmpa <alexmpa@yandex.ru>
2023-10-05 15:12:00 -0300

    Remove breadcrumbs

--- src/components/layout/Breadcrumbs/index.tsx
+++ src/components/layout/Breadcrumbs/index.tsx
@@ -20,43 +20,14 @@ export const Breadcrumbs = ({ tab, route }: BreadcrumbsProps) => {
   return (
     <Box
       pt={{
-        xs: route === '/nexus' ? 4 : 1,
-        lg: route === '/nexus' ? 7.5 : 3.75,
+        xs: 1,
+        lg: 3.75,
       }}
       pb={1.5}
     >
-      <Box pb={1} display={route === '/nexus' ? 'none' : 'inline-block'}>
-        <Link
-          variant="body1"
-          href={routes['/nexus'](echoActiveId as Locales, {
-            tab,
-          })}
-          sx={{
-            color: '#fff',
-            fontWeight: 600,
-            textDecoration: 'underline',
-          }}
-        >
-          <Grid container alignItems="center" spacing={1}>
-            <Grid item>
-              <Box pt={0.3}>
-                <Typography
-                  variant="body1"
-                  sx={{ color: '#fff', fontWeight: 600 }}
-                >
-                  <ArrowBackIcon />
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item>
-              <Text>{echo('goback')}</Text>
-            </Grid>
-          </Grid>
-        </Link>
-      </Box>
       <Box>
         <Text
-          variant={route === '/nexus' ? 'h4' : 'h5'}
+          variant= 'h6'
           sx={{ color: '#fff' }}
           value={echo(`details-${query?.view as string}`)}
         />
