a1d99e37a5952291943c0f2344301dce86badb23
alexmpa <alexmpa@yandex.ru>
2024-02-12 17:42:55 -0300

    Change date formats, now include seconds

--- src/cfg/formats.ts
+++ src/cfg/formats.ts
@@ -1,9 +1,9 @@
 export const NUMBER_FORMAT = '0,0.[00000000]';
 
-export const DATE_FORMAT = 'hh:mm A - DD MMMM YYYY z';
-export const DATE_FORMAT_UTC = 'hh:mm A - DD MMMM YYYY UTC';
-export const DATE_SHORT = `hh:mm A - DD MMM 'YY`;
+export const DATE_FORMAT = 'DD-MMM-YYYY hh:mm:ss A z';
+export const DATE_FORMAT_UTC = 'DD-MMM-YYYY hh:mm:ss A UTC';
+export const DATE_SHORT = `DD-MMM-'YY hh:mm A`;
 
-export const DATE_FORMAT_24 = 'HH:mm - DD MMMM YYYY z';
-export const DATE_FORMAT_UTC_24 = 'HH:mm - DD MMMM YYYY UTC';
-export const DATE_SHORT_24 = `HH:mm - DD MMM 'YY`;
+export const DATE_FORMAT_24 = 'DD-MMM-YYYY HH:mm:ss z';
+export const DATE_FORMAT_UTC_24 = 'DD-MMM-YYYY HH:mm:ss UTC';
+export const DATE_SHORT_24 = `DD-MMM-'YY HH:mm`;
