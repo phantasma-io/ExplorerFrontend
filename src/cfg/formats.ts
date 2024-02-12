a5d88a8c42ed206e92e1fdaa175b44b296236ef7
alexmpa <alexmpa@yandex.ru>
2024-02-12 19:00:49 -0300

    Tweak short version of date

--- src/cfg/formats.ts
+++ src/cfg/formats.ts
@@ -2,8 +2,8 @@ export const NUMBER_FORMAT = '0,0.[00000000]';
 
 export const DATE_FORMAT = 'DD-MMM-YYYY hh:mm:ss A z';
 export const DATE_FORMAT_UTC = 'DD-MMM-YYYY hh:mm:ss A UTC';
-export const DATE_SHORT = `DD-MMM-'YY hh:mm A`;
+export const DATE_SHORT = `hh:mm:ss A DD MMM 'YY`;
 
 export const DATE_FORMAT_24 = 'DD-MMM-YYYY HH:mm:ss z';
 export const DATE_FORMAT_UTC_24 = 'DD-MMM-YYYY HH:mm:ss UTC';
-export const DATE_SHORT_24 = `DD-MMM-'YY HH:mm`;
+export const DATE_SHORT_24 = `HH:mm:ss DD MMM 'YY`;
