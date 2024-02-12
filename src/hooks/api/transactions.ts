97ceeb769427f4ccff8904789539dcc2fb654584
alexmpa <alexmpa@yandex.ru>
2024-02-12 14:52:47 -0300

    Add date to transaction details

--- src/hooks/api/transactions.ts
+++ src/hooks/api/transactions.ts
@@ -3,6 +3,7 @@ import { useEcho } from '@ricardojrmcom/echo';
 import { TableDisplayRow, TableDisplayCol } from 'types/table';
 import { TransactionResults } from 'types/api';
 import { decodeBase16 } from 'scripts/decodeBase16';
+import { unixmsToDate } from 'scripts';
 
 export const useTransactionData = (
   data?: TransactionResults,
@@ -52,6 +53,13 @@ export const useTransactionData = (
         showDesktop: true,
       },
       {
+        id: 'date',
+        label: echo('date'),
+        type: 'date',
+        size: 2,
+        showDesktop: true,
+      },
+      {
         id: 'result',
         label: echo('result'),
         type: 'monospace',
@@ -85,6 +93,7 @@ export const useTransactionData = (
         item?.hash,
         item?.block_hash,
         item?.block_height,
+        item?.date ? unixmsToDate(item.date) : undefined,
         item?.result,
         item?.payload ? decodeBase16(item?.payload) : null,
         item?.state === 'Halt' ? 'Successful' : item?.state,
