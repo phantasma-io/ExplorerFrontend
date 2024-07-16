28f0df4a38ff397d7c1ea7fa805e88721caa977c
Alex <alexmpa@yandex.ru>
2024-07-16 19:14:43 -0300

    Update rpc

--- src/cfg/endpoints/index.ts
+++ src/cfg/endpoints/index.ts
@@ -6,7 +6,7 @@ const parseParams = (params: ApiParams | undefined) =>
 
 const testnet = process.env.NEXT_PUBLIC_TESTNET === 'testnet';
 
-export const url = `https://${testnet ? 'testnet-backend-explorer' : 'api-explorer'}.phantasma.info/api/v1`;
+export const url = `https://api-testnet-explorer.phantasma.info/api/v1`;
 
 export const endpoints: ExplorerApi = {
   '/addresses': (params) => `${url}/addresses${parseParams(params)}`,
